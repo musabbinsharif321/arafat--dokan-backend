26a19c59708cd3b866facf2b38d35b2fbd8a7653
Musab Khan <musabbinsharif321@gmail.com>
2026-07-30 16:25:07 +0600

    Allow Vercel domain https://dokan-nu.vercel.app in CORS settings

--- dokan_backend/settings.py
+++ dokan_backend/settings.py
@@ -100,6 +100,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = False
+CORS_ALLOWED_ORIGINS = [
+    'https://dokan-nu.vercel.app',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
